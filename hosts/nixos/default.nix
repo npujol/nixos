65c0f70b388d97a33d2a2c82e4b731615efe6da3
npujol <naivy.luna@gmail.com>
2024-06-02 22:18:51 +0200

    Rename variables from the old config

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -17,12 +17,12 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "altgr-intl";
+    xkb.layout = "us";
+    xkb.variant = "altgr-intl";
   };
 }
