0c58bb7bf152f3c34a2dc2fd7514ac75ad0590df
npujol <naivy.luna@gmail.com>
2024-08-05 16:08:13 +0200

    Update services.desktopManager.plasma6.enable

--- hosts/limbo/default.nix
+++ hosts/limbo/default.nix
@@ -18,7 +18,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -18,8 +18,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
+  services.desktopManager.plasma5.enable = true;
+  
   # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
