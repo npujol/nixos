c507a9be70197d3f45e24dfdd55c7bee566aecef
npujol <naivy.luna@gmail.com>
2024-08-05 21:42:57 +0200

    Fix plasma5 config

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -18,8 +18,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma5.enable = true;
-  
+  services.xserver.desktopManager.plasma5.enable = true;  
   # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
