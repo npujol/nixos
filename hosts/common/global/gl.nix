8fab15265e7a36a35b3ac10a865d50f5412e9115
npujol <naivy.luna@gmail.com>
2024-11-08 15:18:50 +0100

    remove evaluation warning from hardware.opengl

--- hosts/common/global/gl.nix
+++ hosts/common/global/gl.nix
@@ -5,9 +5,8 @@
   inputs,
   ...
 }: {
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport32Bit = true;
     extraPackages = [unstablePkgs.vulkan-validation-layers];
     # package = unstablePkgs.mesa.drivers;
     # package32 = unstablePkgs.pkgsi686Linux.mesa.drivers;
