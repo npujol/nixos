432b312c4e102b582b1c6acd71579c66a5b9fd98
npujol <naivy.luna@gmail.com>
2024-11-25 13:55:01 +0100

    Update docker root folder

--- hosts/common/features/docker.nix
+++ hosts/common/features/docker.nix
@@ -5,4 +5,7 @@
   #   setSocketVariable = true;
   # };
   virtualisation.docker.autoPrune.enable = true;
+  virtualisation.docker.daemon.settings = {
+  data-root = "/home/nainai/docker_root/";
+};
 }
