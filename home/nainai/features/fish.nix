4602b2104e8239977a92b1d628dd7d3a49c25a18
npujol <naivy.luna@gmail.com>
2025-01-19 22:51:20 +0100

    Update fish

--- home/nainai/features/fish.nix
+++ home/nainai/features/fish.nix
@@ -11,7 +11,7 @@
     shellAliases = {
       # Include here other aliases that you want to use only with fish
       "open" = "command xdg-open";
-      "switch-system" = "sudo nixos-rebuild switch --flake .";
+      "smwitch-syste" = "sudo nixos-rebuild switch --flake .";
       "switch-home" = "home-manager switch --flake . -b backup";
     };
   };
