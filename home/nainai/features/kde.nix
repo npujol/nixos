1f186326260bf04c0be40f7e8ef18a8d2090a694
npujol <naivy.luna@gmail.com>
2025-02-03 21:09:54 +0100

    Fix tdrop command

--- home/nainai/features/kde.nix
+++ home/nainai/features/kde.nix
@@ -8,10 +8,6 @@
     #
     workspace = {
       lookAndFeel = "org.kde.breezedark.desktop";
-      # cursor = {
-      #   theme = "Bibata-Modern-Ice";
-      #   size = 32;
-      # };
       iconTheme = "Breeze-Dark";
       wallpaper = "${./files/desktop.jpg}";
     };
@@ -19,7 +15,7 @@
     hotkeys.commands."launch-kitty" = {
       name = "Launch Kitty";
       key = "F12";
-      command = "tdrop -ma -w -4 -h 80%  kitty -o hide_window_decorations=yes -o background_opacity=0.8";
+      command = "tdrop -mt -w 100% -h 80%  kitty -o hide_window_decorations=yes -o background_opacity=0.8";
     };
   };
 }
