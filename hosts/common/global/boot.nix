05188c5fe98b0556a2f2d948072ab0b52c98d403
npujol <naivy.luna@gmail.com>
2025-05-05 23:35:41 +0200

    Frezze kernel version

--- hosts/common/global/boot.nix
+++ hosts/common/global/boot.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_12;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     blacklistedKernelModules = ["hid-nintendo"];
