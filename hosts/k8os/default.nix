25dc411b3342de9f802398d9b49587ba29ab463c
npujol <naivy.luna@gmail.com>
2025-10-14 22:21:48 +0200

    chore: control energy profiles

--- hosts/k8os/default.nix
+++ hosts/k8os/default.nix
@@ -13,8 +13,7 @@
 
   programs.fish.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  tuned.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
