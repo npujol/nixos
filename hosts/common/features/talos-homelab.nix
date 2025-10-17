ed53bfc518463ec68417bc5b823505317e4a5435
Naivy Pujol Méndez <naivy.luna@gmail.com>
2025-10-17 17:20:58 -0400

    chore: add libvirtd to k8os group

--- hosts/common/features/talos-homelab.nix
+++ hosts/common/features/talos-homelab.nix
@@ -1,94 +1,26 @@
-{ config, pkgs, lib, ... }:
+{{ config, pkgs, ... }:
 
 {
-  # Enable virtualization
+  # Enable virtualization with libvirt
   virtualisation.libvirtd = {
     enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
-    };
+    qemu.package = pkgs.qemu_kvm;
   };
 
-  # Enable KVM nested virtualization for AMD
-  boot.extraModprobeConfig = ''
-    options kvm_amd nested=1
-  '';
+  # Enable KVM for AMD
+  boot.extraModprobeConfig = "options kvm_amd nested=1";
 
-  # Required packages for managing Talos cluster
+  # Install tools
   environment.systemPackages = with pkgs; [
-    # Kubernetes tools
+    virt-manager
     kubectl
     kubernetes-helm
-    
-    # Talos CLI
     talosctl
-    
-    # VM management
-    virt-manager
-    virtiofsd
-    
-    # Network tools
-    bridge-utils
-    dnsmasq
-    
-    # Utilities
-    curl
-    wget
-    jq
   ];
 
-  # Add your user to libvirt group
-  users.users.k8os = {
-    extraGroups = [ "libvirtd" ];
-  };
-
-  # Configure networking for VMs
-  networking = {
-    bridges = {
-      br0 = {
-        interfaces = [ ];
-      };
-    };
-    
-    interfaces = {
-      br0 = {
-        ipv4.addresses = [{
-          address = "192.168.100.1";
-          prefixLength = 24;
-        }];
-      };
-    };
-    
-    nat = {
-      enable = true;
-      internalInterfaces = [ "br0" ];
-      externalInterface = "enp0s31f6";  # Adjust to your interface
-    };
-
-    firewall = {
-      enable = true;
-      trustedInterfaces = [ "br0" ];
-      allowedTCPPorts = [ 6443 ];  # Kubernetes API
-    };
-  };
-
-  # dnsmasq for DHCP on bridge
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      interface = "br0";
-      bind-interfaces = true;
-      dhcp-range = [ "192.168.100.10,192.168.100.100,24h" ];
-      dhcp-option = [ "3,192.168.100.1" ];  # Gateway
-    };
+  # Allow libvirt networking
+  networking.firewall = {
+    checkReversePath = false;
+    trustedInterfaces = [ "virbr0" ];
   };
 }
--- hosts/common/users/k8os.nix
+++ hosts/common/users/k8os.nix
@@ -9,7 +9,7 @@ in {
   users.users.k8os = {
     isNormalUser = true;
     description = "k8os";
-    extraGroups = ["networkmanager" "wheel" "docker"];
+    extraGroups = ["networkmanager" "wheel" "docker" "libvirtd"];
     shell = pkgs.fish;
   };
 }
