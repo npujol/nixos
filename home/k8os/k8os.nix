d93f112bad7e4a96000d9ba4448e863784a95e97
npujol <naivy.luna@gmail.com>
2025-10-22 22:47:32 +0200

    chore: add talos-vms

--- home/k8os/k8os.nix
+++ home/k8os/k8os.nix
@@ -5,11 +5,13 @@
 }: {
   imports = [
     ./common.nix
+    ./features/talos-vms.nix
   ];
   #--------------------------------------------------------------------------
   # Here are all the packages that should only be in the desktop configuration
   #--------------------------------------------------------------------------
   home.packages = with pkgs; [
+    libvirt
     virt-manager
     kubectl
     kubernetes-helm
