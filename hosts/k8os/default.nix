614a5842f046df693b8cc4055d753c87df6136ad
npujol <naivy.luna@gmail.com>
2025-10-26 16:07:16 +0100

    chore: configure immich

--- hosts/k8os/default.nix
+++ hosts/k8os/default.nix
@@ -33,4 +33,24 @@
     xkb.layout = "us";
     xkb.variant = "altgr-intl";
   };
+  services.immich.enable = true;
+  services.immich.port = 2283;
+  users.users.immich.extraGroups = ["video" "render"];
+
+  networking.firewall.allowedTCPPorts = [80];
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."k8os" = {
+    default = true;
+    locations."/" = {
+      proxyPass = "http://[::1]:${toString config.services.immich.port}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        client_max_body_size 50000M;
+        proxy_read_timeout   600s;
+        proxy_send_timeout   600s;
+        send_timeout         600s;
+      '';
+    };
+  };
 }
