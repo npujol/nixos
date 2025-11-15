c1b3609e98993d3a24697a5d69a43a3a5e9239c0
npujol <naivy.luna@gmail.com>
2025-11-15 11:17:09 +0100

    fix(nix.nix): update garbage collection configuration and add new program to run NH with persistent logs

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -13,10 +13,11 @@
       experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-    };
+    # gc = {
+    #
+    #   automatic = true;
+    #   dates = "weekly";
+    # };
     # Add each flake input as a registry
     # To make nix3 commands consistent with the flake
     registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
@@ -24,5 +25,11 @@
     # Map registries to channels
     # Very useful when using legacy commands
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  };
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
   };
 }
