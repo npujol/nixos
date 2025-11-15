f6d81444ecdeca2109e69ee77ea6acd5c79dfef1
npujol <naivy.luna@gmail.com>
2025-11-15 11:09:54 +0100

    fix(boot.nix): update kernel package selection and enable systemd-boot loader configuration

--- hosts/common/global/boot.nix
+++ hosts/common/global/boot.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_16;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     blacklistedKernelModules = ["hid-nintendo"];
