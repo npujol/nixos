6eb827f20136f7ba8a0d99233c77a89de290d6d0
npujol <naivy.luna@gmail.com>
2025-11-16 15:31:53 +0100

    feat(gtk.nix): enable GTK3 and configure default theme and font to Papirus-Dark and Inter respectively

---
+++ home/common/features/gtk.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  gtk = {
+    gtk2.enable = false;
+    enable = true;
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+    font = {
+      package = pkgs.inter;
+      name = "Inter";
+    };
+  };
+}
