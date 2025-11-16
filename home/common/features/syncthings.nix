a25b41f800362ac21e6a8ce5f995d56ea624eeeb
npujol <naivy.luna@gmail.com>
2025-11-16 15:34:04 +0100

    feat(syncthings): enable syncthing service and tray, and add workaround for KDE target

---
+++ home/common/features/syncthings.nix
@@ -0,0 +1,9 @@
+  services.syncthing.enable = true; # Enable the syncthing service
+  services.syncthing.tray.enable = true; # Enable the syncthing tray
+  # Workaround to add a target in KDE
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
