27e539422c9393db8e17078fde2ea357e774822f
npujol <naivy.luna@gmail.com>
2026-04-12 14:40:11 +0200

    fix(limbo.nix): remove redundant configuration for desktop-specific packages from the default configuration

--- home/nainai/limbo.nix
+++ home/nainai/limbo.nix
@@ -7,17 +7,14 @@
     ./common.nix
     ./pi.nix
   ];
-  #--------------------------------------------------------------------------
-  # Here are all the packages that should only be in the desktop configuration
-  #--------------------------------------------------------------------------
   home.packages = with pkgs; [
     cheese
     myPkgs.eden-emu
-    krita
-    pdftk
-    sqlitebrowser
     gimp
-    sops
+    krita
     llama-cpp-vulkan
+    pdftk
+    sops
+    sqlitebrowser
   ];
 }
