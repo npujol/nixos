eef4882daf658f7d3664fd7b7e2978f91119fc41
npujol <naivy.luna@gmail.com>
2026-04-12 12:02:27 +0200

    fix(docker.nix): enable automatic docker container pruning and configure data root directory

--- hosts/common/features/docker.nix
+++ hosts/common/features/docker.nix
@@ -1,11 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   virtualisation.docker.enable = true;
-  # virtualisation.docker.rootless = {
-  #   enable = true;
-  #   setSocketVariable = true;
-  # };
+
   virtualisation.docker.autoPrune.enable = true;
   virtualisation.docker.daemon.settings = {
-  data-root = "/home/nainai/docker_root/";
-};
+    data-root = "/home/nainai/docker_root/";
+  };
 }
