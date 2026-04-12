9ef2c5d50c71922ea9a0cd45ec6a520890d0ed22
npujol <naivy.luna@gmail.com>
2026-04-12 14:05:23 +0200

    fix(syncthing.nix): enable syncthing service and tray, and add workaround for KDE target

--- home/common/features/syncthing.nix
+++ home/common/features/syncthing.nix
@@ -1,15 +1,4 @@
-{lib, ...}: {
-  # services.trayscale = {
-  #   enable = true;
-  # };
-
-  services.syncthing.enable = true; # Enable the syncthing service
-  services.syncthing.tray.enable = true; # Enable the syncthing tray
-  # Workaround to add a target in KDE
-  systemd.user.targets.tray = {
-    Unit = {
-      Description = "Home Manager System Tray";
-      Requires = ["graphical-session-pre.target"];
-    };
-  };
+{...}: {
+  services.syncthing.enable = true;
+  services.syncthing.tray.enable = true;
 }
