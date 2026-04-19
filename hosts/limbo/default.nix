6267f112009c1b88e53d79cf9f94d762fd8f644a
npujol <naivy.luna@gmail.com>
2026-04-19 20:39:14 +0200

    feat(hosts/limbo/default.nix): add openrgb service for controlling RGB on sleep and wake
    * Added `services.hardware.openrgb` to enable the openrgb service with motherboard set to "amd"
    * Added `systemd.services.rgb-sleep-wake` to disable RGB on sleep
    * Added `systemd.services.rgb-wake` to restart RGB on wake

--- hosts/limbo/default.nix
+++ hosts/limbo/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../common/global
@@ -16,5 +16,31 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+  };
+  services.hardware.openrgb = {
+    enable = true;
+    motherboard = "amd";
+  };
+
+  systemd.services.rgb-sleep-wake = {
+    description = "Disable RGB on sleep, restart on wake";
+    wantedBy = ["sleep.target" "suspend.target"];
+    after = ["sleep.target" "suspend.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.openrgb}/bin/openrgb --off";
+      RemainAfterExit = true;
+    };
+  };
+
+  systemd.services.rgb-wake = {
+    description = "Restart RGB on wake";
+    wantedBy = ["suspend.target"];
+    after = ["suspend.target" "sleep.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.openrgb}/bin/openrgb --preset default";
+      RemainAfterExit = true;
+    };
   };
 }
